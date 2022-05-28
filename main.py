29f58d18e6f7a5abf680c069908239256147ad1a
Ranya Al <95483934+110953smc@users.noreply.github.com>
2022-05-28 19:47:45 +0000

    colorama toegevoegd

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+from colorama import Fore
 print("Welkom bij wordle!")
-
-# invoer = input("druk op start")
-
-# if invoer == "start":
-#   print("yay")
-# else:
-#   print("meh")
+print("De computer zal willekeurig een nederlands woord kiezen van vijf letters. Hoe het werkt: \n\n-Typ een woord van vijf letters\n-Als een letter in het woord staat maar niet op de juiste plaats, wordt het " + Fore.YELLOW + "GEEL" + Fore.WHITE + "\n-Als een letter in het woord voorkomt en op de juiste plaats staat, wordt deze " + Fore.GREEN + "GROEN" + Fore.WHITE + "  ")
 
 woorden = ["aap","noot","mies"]
 
-#hoe doe ik random in python
+
 
 gekozenwoord = woorden[1]
 
