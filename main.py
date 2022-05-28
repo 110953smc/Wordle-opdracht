7795338d952b7240995aa1f563cb3556c8c96713
Ranya Al <95483934+110953smc@users.noreply.github.com>
2022-05-28 20:38:45 +0000

    aantal 5 letter woorden in word.txt toegevoegd, functie aangemaakt dat een random woord kiest van te voren uit die lijst

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import random
 from colorama import Fore
 def print_menu():
   print("Welkom bij wordle!")
   print("De computer zal willekeurig een nederlands woord kiezen van vijf letters. Hoe het werkt: \n\n-Typ een woord van vijf letters\n-Als een letter in het woord staat maar niet op de juiste plaats, wordt het " + Fore.YELLOW + "GEEL" + Fore.WHITE + "\n-Als een letter in het woord voorkomt en op de juiste plaats staat, wordt deze " + Fore.GREEN + "GROEN" + Fore.WHITE + "\n-Als een letter helemaal niet in het woord voorkomt, blijft hij wit.\n-Je hebt zes kansen. ")
+
+def read_random_word():
+  with open("words.txt") as f:
+    words = f.read().splitlines()
+    return random.choice(words)
+    
 print_menu()
 
+woord = read_random_word()
 
