98f98d0c4591617e981a466500970943372cb372
Ranya Al <95483934+110953smc@users.noreply.github.com>
2022-05-28 20:13:43 +0000

    words.txt aangemaakt

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 from colorama import Fore
-print("Welkom bij wordle!")
-print("De computer zal willekeurig een nederlands woord kiezen van vijf letters. Hoe het werkt: \n\n-Typ een woord van vijf letters\n-Als een letter in het woord staat maar niet op de juiste plaats, wordt het " + Fore.YELLOW + "GEEL" + Fore.WHITE + "\n-Als een letter in het woord voorkomt en op de juiste plaats staat, wordt deze " + Fore.GREEN + "GROEN" + Fore.WHITE + "  ")
+def print_menu():
+  print("Welkom bij wordle!")
+  print("De computer zal willekeurig een nederlands woord kiezen van vijf letters. Hoe het werkt: \n\n-Typ een woord van vijf letters\n-Als een letter in het woord staat maar niet op de juiste plaats, wordt het " + Fore.YELLOW + "GEEL" + Fore.WHITE + "\n-Als een letter in het woord voorkomt en op de juiste plaats staat, wordt deze " + Fore.GREEN + "GROEN" + Fore.WHITE + "\n-Als een letter helemaal niet in het woord voorkomt, blijft hij wit.\n-Je hebt zes kansen. ")
+print_menu()
 
-woorden = ["aap","noot","mies"]
 
-
-
-gekozenwoord = woorden[1]
-
-woord = input("schrijf een woord: \n")
-
-if gekozenwoord == woord:
-  print("goed geraden")
-else:
-  print("fout!")
-
-  
-print(gekozenwoord)
-print(woord)
