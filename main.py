13c4c8ff34895114ef507f9f0cc2d17bbe7f53d1
Marina Leenhouts <95483938+111069smc@users.noreply.github.com>
2022-06-03 13:21:12 +0000

    als woord meer dan 5 letter is krijgt speler melding

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 import random
 from colorama import Fore
 
+
 def print_menu():
-  print("Welkom bij wordle!")
-  print("De computer zal willekeurig een nederlands woord kiezen van vijf letters. Hoe het werkt: \n\n-Typ een woord van vijf letters\n-Als een letter in het woord staat maar niet op de juiste plaats, wordt het " + Fore.YELLOW + "GEEL" + Fore.WHITE + "\n-Als een letter in het woord voorkomt en op de juiste plaats staat, wordt deze " + Fore.GREEN + "GROEN" + Fore.WHITE + "\n-Als een letter helemaal niet in het woord voorkomt, blijft hij wit.\n-Je hebt zes kansen. ")
+    print("Welkom bij wordle!")
+    print(
+        "De computer zal willekeurig een nederlands woord kiezen van vijf letters. Hoe het werkt: \n\n-Typ een woord van vijf letters\n-Als een letter in het woord staat maar niet op de juiste plaats, wordt het "
+        + Fore.YELLOW + "GEEL" + Fore.WHITE +
+        "\n-Als een letter in het woord voorkomt en op de juiste plaats staat, wordt deze "
+        + Fore.GREEN + "GROEN" + Fore.WHITE +
+        "\n-Als een letter helemaal niet in het woord voorkomt, blijft hij wit.\n-Je hebt zes kansen. "
+    )
+
 
 def raad_systeem(correct):
-  raad = input(Fore.WHITE + "kies een woord: \n")
+    raad = input(Fore.WHITE + "kies een woord: \n")
 
-  if len(raad) != 5:
-    print("mag niet")
+    if len(raad) != 5:
+        raad = str(
+            input(
+                "Zorg ervoor dat het woord 5 letters is. \n- kies een woord: ")
+        )
 
-  if raad == correct:
-    print("Yay gewonnen!")
-  
-  for letter in range(len(raad)):
-    if raad[letter] == correct[letter]:
-      print(Fore.GREEN, raad[letter])
-    elif raad[letter] in correct:
-      print(Fore.YELLOW, raad[letter])
-    else:
-      print(Fore.WHITE, raad[letter])
+    if raad == correct:
+        print("Yay gewonnen!")
 
-  
-  raad_systeem(correct)
+    for letter in range(len(raad)):
+        if raad[letter] == correct[letter]:
+            print(Fore.GREEN, raad[letter])
+        elif raad[letter] in correct:
+            print(Fore.YELLOW, raad[letter])
+        else:
+            print(Fore.WHITE, raad[letter])
 
-  
+    raad_systeem(correct)
+
+
 print_menu()
 
 woordenlijst = ("words.txt")
