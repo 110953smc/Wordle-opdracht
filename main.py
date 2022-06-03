d7bae73ce9fe0314682d744df9d9e8bd7d8b9492
Ranya Al <95483934+110953smc@users.noreply.github.com>
2022-06-03 12:44:50 +0000

    bug fixes

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import random
 from colorama import Fore
+
 def print_menu():
   print("Welkom bij wordle!")
   print("De computer zal willekeurig een nederlands woord kiezen van vijf letters. Hoe het werkt: \n\n-Typ een woord van vijf letters\n-Als een letter in het woord staat maar niet op de juiste plaats, wordt het " + Fore.YELLOW + "GEEL" + Fore.WHITE + "\n-Als een letter in het woord voorkomt en op de juiste plaats staat, wordt deze " + Fore.GREEN + "GROEN" + Fore.WHITE + "\n-Als een letter helemaal niet in het woord voorkomt, blijft hij wit.\n-Je hebt zes kansen. ")
 
-def read_random_word():
-  with open("words.txt") as f:
-    words = f.read().splitlines()
-    return random.choice(words)
-    
+def raad_systeem(correct):
+  raad = input("kies een woord: \n")
+
+  if len(raad) != 5:
+    print("mag niet")
+
+  if raad == correct:
+    print("Yay gewonnen!")
+  
+  for letter in range(len(raad)):
+    if raad[letter] == correct[letter]:
+      print(Fore.GREEN, raad[letter])
+    elif raad[letter] in correct:
+      print(Fore.YELLOW, raad[letter])
+    else:
+      print(Fore.WHITE, raad[letter])
+
+  
+  raad_systeem(correct)
+
+  
 print_menu()
 
-woord = read_random_word()
+woordenlijst = ["aapje","noots","miesp"] # todo lijst uit txt halen
+woord = woordenlijst[0] # todo: random woord
 
+raad_systeem(woord)
