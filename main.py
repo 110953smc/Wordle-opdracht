8fe1b7372f9bd9684b1fe4091693735665837595
Marina Leenhouts <95483938+111069smc@users.noreply.github.com>
2022-06-03 13:08:19 +0000

    "kies een woord" is nu altijd wit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def print_menu():
   print("De computer zal willekeurig een nederlands woord kiezen van vijf letters. Hoe het werkt: \n\n-Typ een woord van vijf letters\n-Als een letter in het woord staat maar niet op de juiste plaats, wordt het " + Fore.YELLOW + "GEEL" + Fore.WHITE + "\n-Als een letter in het woord voorkomt en op de juiste plaats staat, wordt deze " + Fore.GREEN + "GROEN" + Fore.WHITE + "\n-Als een letter helemaal niet in het woord voorkomt, blijft hij wit.\n-Je hebt zes kansen. ")
 
 def raad_systeem(correct):
-  raad = input("kies een woord: \n")
+  raad = input(Fore.WHITE + "kies een woord: \n")
 
   if len(raad) != 5:
     print("mag niet")
@@ -28,7 +28,7 @@ def raad_systeem(correct):
   
 print_menu()
 
-woordenlijst = ["aapje","noots","miesp"] # todo lijst uit txt halen
+woordenlijst = ["agent","atoom","bloed","traan"] # todo lijst uit txt halen
 woord = woordenlijst[0] # todo: random woord
 
 raad_systeem(woord)
